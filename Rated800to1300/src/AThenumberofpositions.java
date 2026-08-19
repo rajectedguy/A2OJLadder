1ed43c854d91e43685001a16698daa724ba8c7a8
rajectedguy <rajectedguy@gmail.com>
2026-08-19 23:14:44 +0530

    50 - 51  < 1300

---
+++ Rated800to1300/src/AThenumberofpositions.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class AThenumberofpositions {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int start = Math.max(a + 1, n - b);
+        int ans = n - start + 1;
+        System.out.println(ans);
+    }
+}
